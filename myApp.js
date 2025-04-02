7a4f9e75d9da91b1ca042b3adc94752b2d9ff23d
luis04-laus Uribe <luisangeluribesantiago@gmail.com>
2025-04-02 15:35:05 +0000

    Add initial application

--- myApp.js
+++ myApp.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const app = express();
+const helmet = require("helmet");
+const app = express();
 
 
-
-
-
-
+const PORT = process.env.PORT || 3030;
+app.listen(PORT, () => {
+console.log('Useful Programmer Info Security started on port ${PORT}');
+});
 
 
 
